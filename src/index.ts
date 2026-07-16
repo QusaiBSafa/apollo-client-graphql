e65f933711447da2cfd1990b0f3aa04b1e675535
Qusai Safa <153466377+QusaiBSafa@users.noreply.github.com>
2026-07-16 03:02:58 +0300

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
